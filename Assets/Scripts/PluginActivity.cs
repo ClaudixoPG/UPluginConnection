d4c7c743a0f156f3beccb93557f9112a6fdbbca8
Claudio Rubio <43611630+ClaudixoPG@users.noreply.github.com>
2025-05-12 19:35:24 -0400

    Message Sender and Receiver Working

--- Assets/Scripts/PluginActivity.cs
+++ Assets/Scripts/PluginActivity.cs
@@ -1,19 +1,19 @@
+using TMPro;
 using UnityEngine;
 
 public class PluginActivity : MonoBehaviour
 {
+    //Private Variables
     AndroidJavaObject _pluginActivity;
+    //Public Variables
+    public TextMeshProUGUI messageToSend;
+    public TextMeshProUGUI messageReceived;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        _pluginActivity = new AndroidJavaObject("com.ra.uactivity.PluginActivity");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _pluginActivity = new AndroidJavaObject("com.randomadjective.uactivity.PluginActivity");
     }
 
     public void Add()
@@ -31,5 +31,21 @@ public class PluginActivity : MonoBehaviour
         {
             _pluginActivity.Call("ShowToast", "Hello from Unity!");
         }
+    }
+
+    public void SendMessage()
+    {
+        if (_pluginActivity != null)
+        {
+            var message = messageToSend.text;
+            Debug.Log("Message to send: " + message);
+            _pluginActivity.Call("sendMessageToSmartwatch", message);
+        }
+    }
+
+    public void OnMessageReceived(string message)
+    {
+        Debug.Log("Mensaje recibido en Unity: " + message);
+        messageReceived.text = "Message Received: " + message;
     }
 }
